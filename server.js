690b90281864c9134ed8ba101a6764d4fdbe2ef0
Sushanth <s.sushanth.reddy49@gmail.com>
2025-06-03 21:34:32 -0400

    Added Dockerfile and actions file

--- server.js
+++ server.js
@@ -8,10 +8,10 @@ import { connectToDatabase } from './db/connection.js';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 const corsOptions = {
-  origin: 'http://localhost:5173', // Replace with your frontend's URL
+  origin: 'http://localhost:3000', // Replace with your frontend's URL
   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
   allowedHeaders: ['Content-Type'], // Allowed headers
 };
